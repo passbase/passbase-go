dab20a32fc1c7d1fd7f2792a8195bc47771e6df3
Nicolas Brugneaux <nicolas@passbase.com>
2020-10-15 19:14:04 +0200

    release: v1.0.1

--- configuration.go
+++ configuration.go
@@ -61,7 +61,7 @@ func NewConfiguration() *Configuration {
 	cfg := &Configuration{
 		BasePath:      "https://api.passbase.com/verification/v1",
 		DefaultHeader: make(map[string]string),
-		UserAgent:     "passbase/go@1.0.0",
+		UserAgent:     "passbase/go@1.0.1",
 	}
 	return cfg
 }
